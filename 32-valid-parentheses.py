60a815f33eab14c169e5605d6257e0638d94ff13
Sharif <78465304+sharifkhan96@users.noreply.github.com>
2025-09-23 00:34:12 +0100

    updated version;;;)

--- 32-valid-parentheses.py
+++ 32-valid-parentheses.py
@@ -1,5 +1,6 @@
 class Solution:
-    def longestValidParentheses(self, s: str) -> int:
+    # stack based solution
+    '''def longestValidParentheses(self, s: str) -> int:
         stack = [-1]  # base index to handle edge cases
         maxLength = 0
 
@@ -13,6 +14,40 @@ class Solution:
                 else:
                     # valid substring length = currentIndex - last unmatched index
                     maxLength = max(maxLength, i - stack[-1])
+
+        return maxLength'''
+    
+    # two pointer solution
+    # time comp: O(n)
+    # space comp: O(1)
+    def longestValidParentheses(self, s: str) -> int:
+        left = right = 0
+        maxLength = 0
+
+        # Pass 1: Left to Right
+        for char in s:
+            if char == '(':
+                left += 1
+            else:
+                right += 1
+
+            if left == right:
+                maxLength = max(maxLength, 2 * right)
+            elif right > left:
+                left = right = 0  # reset (invalid substring)
+
+        # Pass 2: Right to Left (to catch unmatched '(' cases)
+        left = right = 0
+        for char in reversed(s):
+            if char == ')':
+                right += 1
+            else:
+                left += 1
+
+            if left == right:
+                maxLength = max(maxLength, 2 * left)
+            elif left > right:
+                left = right = 0  # reset (invalid substring)
 
         return maxLength
 
