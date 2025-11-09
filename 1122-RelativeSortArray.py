00e3c79cd0582f2656a4d07e76f7ed8d7387f9f6
Sharif <78465304+sharifkhan96@users.noreply.github.com>
2025-11-09 00:22:16 +0000

    just a small update on the frequency side :))))

--- 1122-RelativeSortArray.py
+++ 1122-RelativeSortArray.py
@@ -3,16 +3,20 @@ from collections import Counter
 class Solution:
     def relativeSortArray(self, arr1: list[int], arr2: list[int]) -> list[int]:
         # step 1: count frequency of each element in arr1
-        freq = Counter(arr1)
+        freq = {}
+        for item in arr1:
+            freq[item] = freq.get(item, 0) + 1
+        #freq = Counter(arr1)
+        
         result = []
 
-        #step2: add elements following arr2’s order
+        # step2: add elements following arr2’s order
         for num in arr2:
             if num in freq:
                 result.extend([num] * freq[num])
                 del freq[num]
 
-        # step 3: add remaining elements (  not in arr2)
+        # step 3: add remaining elements ( not in arr2)
         for num in sorted(freq.keys()):
             result.extend([num] * freq[num])
 
