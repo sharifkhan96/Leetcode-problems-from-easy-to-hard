130715ee864a9819a4a20b071a5e5428a81fbfe7
Sharif <78465304+sharifkhan96@users.noreply.github.com>
2025-11-20 00:41:39 +0000

    1365: corrected versionnnnnnnnn

--- 1365-HowmanynumbersaresmallerthancurrentNumber.py
+++ 1365-HowmanynumbersaresmallerthancurrentNumber.py
@@ -1,21 +1,36 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: list[int]) -> list[int]:
-        #nums.sort()
-        result = []
-        count = 0
-        for i in range(len(nums)-1):
-            for j in range(1, len(nums)-1):
-                if nums[i] > nums[j]:
-                    count += 1
-                else:
-                    continue
-            result.append(count)
+        sorted_nums = sorted(nums)
+        indexed_map = {}
+        for i, num in enumerate(sorted_nums):
+            if num not in indexed_map:
+                indexed_map[num] = i
+        print(nums)
+        return [indexed_map[num] for num in nums]
+    # time comp: O(n log n)
+    # space comp: o(n)
+
+
+        '''
+        # time comp: O(n^2)
+        # space comp: O(n)
+        # result = []
+        # count = 0
+        # for i in range(len(nums)):
+        #     for j in range(len(nums)):
+        #         if i == j:
+        #             continue
+
+        #         if nums[i] > nums[j]:
+        #             count += 1
+    
+        #     result.append(count)
+        #     count = 0
         
-        return result
+        # return result
+        '''
 
 
-# time comp: O(m*n)
-# space comp: O(n)
 
 
 class main():
