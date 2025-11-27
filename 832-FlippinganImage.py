af65a6bbf99fd406ccca3159a47aaa674d880593
Sharif <78465304+sharifkhan96@users.noreply.github.com>
2025-11-27 00:27:07 +0000

    832-flippinganImage_correctedversionnn;)

--- 832-FlippinganImage.py
+++ 832-FlippinganImage.py
@@ -4,21 +4,32 @@ from itertools import chain
 
 class Solution:
     def flipAndInvertImage(self, image: list[list[int]]) -> list[list[int]]:
-        #rev = image[::-1]
+        
+        # one step approacah
+        for row in image:
+            i, j = 0, len(row) - 1
+            while i <= j:
+                row[i], row[j] = row[j] ^ 1, row[i] ^ 1
+                i += 1
+                j -= 1
+        return image
+    
+    # time comp: O(n*m) ==> O(n)
+    # space comp: O(1)
+        
+        '''
+        # two step approach
+        result = []
         for sublist in image:
-            sublist.reverse()
-        #print(image)
+            reversed_sublist = sublist[::-1]
+            inverted_sublist = [1 - x for x in reversed_sublist]
+            result.append(inverted_sublist)
 
-        res = 0 
-        for num in image:
-            # XOR the current number with the result
-            res ^= num  
-
-        return res
-
-# time comp: O(n)
+        return result
+        
+# time comp: O(n*m) ==> O(n)
 # space comp: O(n)
-
+        '''
 
 class main():
     image = [[1,1,0],[1,0,1],[0,0,0]]
