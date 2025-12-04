2bdc234ca242d39fb8fbc05322ee6575d39ed978
Sharif <78465304+sharifkhan96@users.noreply.github.com>
2025-12-04 21:17:12 +0000

    dp solution of climbing stairs problem

--- 70.ClimbingStairs.py
+++ 70.ClimbingStairs.py
@@ -1,4 +1,37 @@
 class Solution:
+    # dp solution : O(n) & O(n)
+    def climbStairs(self, n: int) -> int:
+
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        
+        dp = [0] * (n+1)
+
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
+
+    '''
+    recursive but not optimal solution:
+    def climbStairs(self, n: int) -> int:
+        # Base case: if there's only 1 step, only 1 way
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        return climbStairs(n - 1) + climbStairs(n - 2)
+    '''
+    
+    '''
     def climbStairs(self, n: int) -> int:
         # Base case: if there's only 1 step, only 1 way
         if n == 1:
@@ -13,6 +46,25 @@ class Solution:
             first, second = second, first + second
         
         return second
+    '''
+
+
+    '''
+    # space optimized solution: # O(n), O(1)
+    if n <= 2:
+        return n
+
+    
+    prev2 = 1
+    prev1 = 2
+
+    for _ in range(3, n+1):
+        current = prev1 + prev2
+        prev2 = prev1
+        prev1 = current
+
+    return prev1
+    '''
 
 
 def main():
