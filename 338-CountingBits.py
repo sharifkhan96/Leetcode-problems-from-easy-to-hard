0a378f6e2f00f8c83b0251427a299f2756e6a593
Sharif <78465304+sharifkhan96@users.noreply.github.com>
2025-12-08 16:05:02 +0000

    338-counting bits solution of O(n log n)

--- 338-CountingBits.py
+++ 338-CountingBits.py
@@ -1,15 +1,33 @@
 class Solution:
     def countBits(self, n: int) -> list[int]:
+
+        # o(n log n) solution
+        # return [i.bit_count() for i in range(n + 1)]
+
+        result = []
+        for i in range(n + 1):
+            count = 0
+
+            x = i
+            while x > 0:
+                count += x & 1
+                x >>= 1
+            result.append(count)
+
+        return result
+
+
+        # dp solution
+        # time comp: O(n)
+        # space comp: O(n)
+        '''
         dp = [0] * (n + 1)
-        
+
         for i in range(1, n + 1):
             dp[i] = dp[i >> 1] + (i & 1)
 
         return dp
-
-
-# time comp: O(n)
-# space comp: O(n)
+        '''
 
 
 class main():
