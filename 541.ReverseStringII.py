5bcc7cc6c83137e1132f3005eb437c0602c3531f
Sharif <78465304+sharifkhan96@users.noreply.github.com>
2025-12-12 00:38:56 +0000

    541-reversestring|| problem

--- 541.ReverseStringII.py
+++ 541.ReverseStringII.py
@@ -7,16 +7,38 @@ class Solution:
             s[i : i + k] = reversed(s[i : i + k])
 
         return "".join(s)
+        # time comp: O(n)
+        # space comp: O(n)
 
+        '''
+        # 2 pointer sol:
+        class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        s = list(s)
+
+        for i in range(0, len(s), 2 * k):
+            left = i
+            right = min(i + k - 1, len(s) - 1)
+
+            # reverse using two pointers
+            while left < right:
+                s[left], s[right] = s[right], s[left]
+                left += 1
+                right -= 1
+
+        return "".join(s)
+
+        '''
 
 
 
 
 def main():
-    input = "abcdefg",
+    input = "abcdefg"
     k = 2
     obj = Solution()
-    print(obj.reverseStr(input, k))
+    print("Our string: ", input, " before reversing by k chars.")
+    print("Our String: ", obj.reverseStr(input, k), " after reversing by k chars.")
 
 if __name__ == "__main__":
     main()
