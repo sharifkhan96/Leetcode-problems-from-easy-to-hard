dc131d5a1ad79ee17adbb6e0cf0f137c8d0429b3
Sharif <78465304+sharifkhan96@users.noreply.github.com>
2025-12-13 23:59:28 +0000

    bin search

---
+++ 704-BinarySearch.py
@@ -0,0 +1,27 @@
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
+
+
+
+def main():
+   nums = [-1,0,3,5,9,12]
+   target = 9
+   obj = Solution()
+   print(obj.findMaxConsecutiveOnes(nums, target))
+
+
+if __name__ == "__main__":
+    main()
