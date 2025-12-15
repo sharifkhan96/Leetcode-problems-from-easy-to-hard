dbf106f1d00600c334c6d9ab87b170ba5fb60191
Sharif <78465304+sharifkhan96@users.noreply.github.com>
2025-12-15 23:59:02 +0000

    1684-countNumsofConsistentStrings

---
+++ 1684-countNumofConsistentStrings.py
@@ -0,0 +1,31 @@
+class Solution:
+    def countConsistentStrings(self, allowed: str, words: list[str]) -> int:
+        allowed_set = set(allowed)
+        count = 0
+
+        for word in words:
+            is_consistent = True
+            for ch in word:
+                if ch not in allowed_set:
+                    is_consistent = False
+                    break
+
+            if is_consistent:
+                count += 1
+
+        return count
+
+# time comp: O(n * m)
+# space comp: O(1)
+
+
+
+class main():
+
+    allowed = "abc"
+    words = ["a","b","c","ab","ac","bc","abc"]
+    obj = Solution()
+    print(obj.countConsistentStrings(allowed, words))
+
+if __name__ == "__main__":
+    main()
