9467c4af3973d390f1dee3e7708c0a67082c0864
Sharif <78465304+sharifkhan96@users.noreply.github.com>
2026-01-08 01:20:27 +0000

    BRUTE FORCE VERSION

--- 2073.TimeNeededtoBuyTickets.py
+++ 2073.TimeNeededtoBuyTickets.py
@@ -1,17 +1,35 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: list[int], k: int) -> int:
-        total_time = 0
-        target_tickets = tickets[k]
+        
+        # total_time = 0
+        # target_tickets = tickets[k]
 
-        for i in range(len(tickets)):
-            if i <= k:
-                total_time += min(tickets[i], target_tickets)
-            else:
-                total_time += min(tickets[i], target_tickets - 1)
+        # for i in range(len(tickets)):
+        #     if i <= k:
+        #         total_time += min(tickets[i], target_tickets)
+        #     else:
+        #         total_time += min(tickets[i], target_tickets - 1)
 
-        return total_time
+        # return total_time
+    
+
 
         
+        # brute force version:
+        time = 0
+        while True:
+            for i in range(len(tickets)):
+                if tickets[k] == 0:
+                    return time
+                if tickets[i] == 0:
+                    continue
+                    
+                if tickets[i] >= 1:
+                    tickets[i] -= 1
+                    time+=1
+            
+        return time
+        # o(M*N)
 
 def main():
     st = Solution()
