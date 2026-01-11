c3521a0547065dec81be85c564d03398aa940ce0
Sharif <78465304+sharifkhan96@users.noreply.github.com>
2026-01-11 00:12:28 +0000

    637-avgoflevelsinbintree

--- 637.AverageofLevelsinBinaryTree.py
+++ 637.AverageofLevelsinBinaryTree.py
@@ -6,13 +6,17 @@ class TreeNode:
         self.right = right
 class Solution:
     def averageOfLevels(self, root) -> list[float]:
+
+        if not root:
+            return
+        
         queue = deque([root])
 
         result = []
 
         while queue:
             level = []
-            for i in range(queue):
+            for i in range(len(queue)):
                 node = queue.popleft()
                 level.append(node.val)
                 if node.left: 
@@ -25,7 +29,20 @@ class Solution:
 
 
 def main():
-    ...
+    
+    root = TreeNode(3)
+    node9 = TreeNode(9)
+    node20 = TreeNode(20)
+    node15 = TreeNode(15)
+    node7 = TreeNode(7)
+
+    root.left = node9
+    root.right = node20
+    node20.left = node15
+    node20.right = node7
+
+    obj = Solution()
+    print(obj.averageOfLevels(root))   # Expected: [3.0, 14.5, 11.0]
 
     
 
