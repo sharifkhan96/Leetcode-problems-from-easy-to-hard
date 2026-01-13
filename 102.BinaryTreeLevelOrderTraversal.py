af5b06a2cbf0c839315601532762179f7ac7c3e2
Sharif <78465304+sharifkhan96@users.noreply.github.com>
2026-01-13 21:41:50 +0000

    102-binary tree level order traversal
    given a binary tree, return a list of all grouped values in each branch...

---
+++ 102.BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,53 @@
+from collections import deque
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def averageOfLevels(self, root) -> list[float]:
+
+        if not root:
+            return []
+        
+
+        queue = deque([root])
+        result = []
+
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            result.append(level)
+        return result
+
+# time and space complexities: O(N) & O(N)
+
+def main():
+    
+    root = TreeNode(3)
+    node9 = TreeNode(9)
+    node20 = TreeNode(20)
+    node15 = TreeNode(15)
+    node7 = TreeNode(7)
+
+    root.left = node9
+    root.right = node20
+    node20.left = node15
+    node20.right = node7
+
+    obj = Solution()
+    print(obj.averageOfLevels(root))   # Expected: [3.0, 14.5, 11.0]
+
+    
+
+
+if __name__ == "__main__":
+    main()
