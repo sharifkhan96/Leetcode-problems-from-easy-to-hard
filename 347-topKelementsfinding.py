1948224d6f555ff3b355cb3d8f43b686aeeb80b1
Sharif <78465304+sharifkhan96@users.noreply.github.com>
2026-02-06 13:32:51 +0000

    347-topkelement2version :))

--- 347-topKelementsfinding.py
+++ 347-topKelementsfinding.py
@@ -3,13 +3,20 @@ import heapq
 
 class Solution:
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
-        # Step 1: Count frequencies
-        freq = Counter(nums)  # e.g. Counter({1:3, 2:2, 3:1})
+        # # Step 1: Count frequencies
+        # freq = Counter(nums)  # e.g. Counter({1:3, 2:2, 3:1})
+        freq = {}
+        for num in nums:
+            if num in freq:
+                freq[num] += 1
+            else:
+                freq[num] = 1
         
         # Step 2: Use a heap to get top k frequent elements
         return heapq.nlargest(k, freq.keys(), key=freq.get)
 
-
+# time complexity: O(n log k) where n is for counting and log k for heap operations
+# space complexity: O(n + k) where n is for freq map and k is for heap
 
 
 
