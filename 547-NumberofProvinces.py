6b98082220cdbb1da9c76b5b11a44d8555d79b17
Sharif <78465304+sharifkhan96@users.noreply.github.com>
2026-02-12 19:50:39 +0000

    added the bfs solution as well implemented via queue

--- 547-NumberofProvinces.py
+++ 547-NumberofProvinces.py
@@ -1,6 +1,7 @@
 from typing import List
 
 class Solution:
+    # dfs solution
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n = len(isConnected)
 
@@ -22,9 +23,38 @@ class Solution:
                 dfs(city)
 
         return provinces
-
-
 # time and space complexities: O(n^2) and O(n)
+
+
+'''
+bfs solution: 
+class Solution:
+    def bfs(self, node, isConnected, visited):
+        from collections import deque
+
+        queue = deque([node])
+        visited[node] = True
+
+        while queue:
+            node = queue.popleft()
+
+            for i in range(len(isConnected)):
+                if isConnected[node][i] == 1 and not visited[i]:
+                    queue.append(i)
+                    visited[i] = True
+
+    def findCircleNum(self, isConnected):
+        n = len(isConnected)
+        numberOfComponents = 0
+        visited = [False] * n
+
+        for i in range(n):
+            if not visited[i]:
+                numberOfComponents += 1
+                self.bfs(i, isConnected, visited)
+
+        return numberOfComponents
+'''
 
 
 def main():
