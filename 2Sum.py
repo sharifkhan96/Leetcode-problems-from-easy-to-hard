8f2b81ac606e98d14574150262e0d160639525b4
Sharif <78465304+sharifkhan96@users.noreply.github.com>
2026-02-18 22:42:09 +0000

    updated version ;;)

--- 2Sum.py
+++ 2Sum.py
@@ -1,20 +1,27 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        # O(n^2)
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
 
+        # O(n)
+        Hashmap1 = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in Hashmap1:
+                return [Hashmap1[diff], i]
+            
+            Hashmap1[n] = i
 
-nums = [2, 7, 11, 15]
-target = [9]
 
 
 
 
 def main():
     nums = [2,7,11,15]
-    target = 9
+    target = 18
     obj1 = Solution()
     result = obj1.twoSum(nums, target)
     print(result)
