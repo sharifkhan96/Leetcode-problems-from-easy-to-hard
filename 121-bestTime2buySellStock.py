cce86cfbef675051989cbbeab398a8c7b7eaef3e
Sharif <78465304+sharifkhan96@users.noreply.github.com>
2026-02-18 23:20:31 +0000

    2nd version ;)

--- 121-bestTime2buySellStock.py
+++ 121-bestTime2buySellStock.py
@@ -1,19 +1,40 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-        min_price = float('inf')  # very large number
+        
+        # 1st approach, two pointer solution
+        l, r = 0, 1 # l = buying, r = selling
+
         max_profit = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                max_profit = max(profit, max_profit)
+            else:
+                l = r
 
-        for price in prices:
-            if price < min_price:
-                min_price = price  # update buy price
-            profit = price - min_price
-            if profit > max_profit:
-                max_profit = profit  # update best profit
-
+            r += 1
         return max_profit
 
+        
+        # 2nd approach
+        # min_price = float('inf')  # very large number
+        # max_profit = 0
+
+        # for price in prices:
+        #     if price < min_price:
+        #         min_price = price  # update buy price
+        #     profit = price - min_price
+        #     if profit > max_profit:
+        #         max_profit = profit  # update best profit
+
+        # return max_profit
+
+        
 # time comp: O(n)
 # space comp: O(1)
+
+
+
 
 def main():
     prices = [7, 1, 5, 3, 6, 4]
