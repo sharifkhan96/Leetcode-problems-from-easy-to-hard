d5528833440b2b2f32304eda8e4f8f40c84da229
Sharif <78465304+sharifkhan96@users.noreply.github.com>
2026-02-21 22:30:21 +0000

    53-kadan's algo solution added

--- 53-MaxSubarray.py
+++ 53-MaxSubarray.py
@@ -2,32 +2,49 @@ class Solution:
     def maxSubArray(self, nums: list[int]) -> int:
 
 
-        # dp sol:
+        # dp solution:
+        # dp = [0] * len(nums)
+        # dp[0] = nums[0]
 
-        dp = [0] * len(nums)
-        dp[0] = nums[0]
+        # for i in range(1, len(nums)):
+        #     dp[i] = max(nums[i], dp[i-1] + nums[i])
 
-        for i, n in enumerate(nums):
-            dp[i] = max(n, dp[i-1]+n)
-
-        return max(dp)
+        # return max(dp)
 
 
-        '''
+        # kadan's algo: time and space complexities: O(n) & O(1)
         maxSum = nums[0]
-        currentSum = 0
+        currentSum = nums[0]
 
-        for i in nums:
-            if currentSum < 0:
-                currentSum = 0
-
-            currentSum += i
-            maxSum = max(maxSum, currentSum)
+        for i in range(1, len(nums)):
+                currentSum = max(nums[i], currentSum + nums[i])
+                maxSum = max(currentSum, maxSum)
 
         return maxSum
+
+
+        
+        # maxSum = nums[0]
+        # currentSum = 0
+        # start = 0
+        # best_start = 0
+        # best_end = 0
+
+        # for i, n in enumerate(nums):
+        #     if currentSum < 0:
+        #         currentSum = 0
+        #         start = i
+
+        #     currentSum += n
+
+        #     if currentSum > maxSum:
+        #         maxSum = currentSum
+        #         best_start = start
+        #         best_end = i
+        # return maxSum, (best_start, best_end)
 # time comp: O(n)
 # space comp: O(n)
-    '''
+    
 
 
 class main():
