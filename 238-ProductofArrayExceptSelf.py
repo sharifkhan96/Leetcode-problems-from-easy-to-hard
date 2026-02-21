08fb4ee72c3592583df9e8f3ed7475303b1192f0
Sharif <78465304+sharifkhan96@users.noreply.github.com>
2026-02-21 00:41:39 +0000

    238-version2.0

--- 238-ProductofArrayExceptSelf.py
+++ 238-ProductofArrayExceptSelf.py
@@ -1,19 +1,35 @@
 class Solution:
     def productExceptSelf(self, nums):
-        n = len(nums)
-        answer = [1] * n
+        # n = len(nums)
+        # answer = [1] * n
 
-        # Step 1: Left products
-        for i in range(1, n):
-            answer[i] = answer[i - 1] * nums[i - 1]
+        # # Step 1: Left products
+        # for i in range(1, n):
+        #     answer[i] = answer[i - 1] * nums[i - 1]
 
-        # Step 2: Right products
-        right_product = 1
-        for i in range(n - 1, -1, -1):
-            answer[i] *= right_product
-            right_product *= nums[i]
+        # # Step 2: Right products
+        # right_product = 1
+        # for i in range(n - 1, -1, -1):
+        #     answer[i] *= right_product
+        #     right_product *= nums[i]
 
-        return answer
+        # return answer
+
+
+        res = [1] * len(nums)
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+
+        return res
+
 
 
 def main():
