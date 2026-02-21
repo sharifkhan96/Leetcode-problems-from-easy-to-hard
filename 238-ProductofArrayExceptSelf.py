1ae2005d99a95de63a993fdc2972efc1a2caa2e8
Sharif <78465304+sharifkhan96@users.noreply.github.com>
2026-02-21 01:08:12 +0000

    238-version3.0

--- 238-ProductofArrayExceptSelf.py
+++ 238-ProductofArrayExceptSelf.py
@@ -1,34 +1,68 @@
 class Solution:
     def productExceptSelf(self, nums):
+        n = len(nums)
+        answer = [1] * n
+
+        # Step 1: Left products
+        for i in range(1, n):
+            answer[i] = answer[i - 1] * nums[i - 1]
+
+        # Step 2: Right products
+        right_product = 1
+        for i in range(n - 1, -1, -1):
+            answer[i] *= right_product
+            right_product *= nums[i]
+
+        return answer
+
+
+        #----------------two pass prefix and postfixes solutions
+        # res = [1] * len(nums)
+
+        # prefix = 1
+        # for i in range(len(nums)):
+        #     res[i] = prefix
+        #     prefix *= nums[i]
+
+        # postfix = 1
+        # for i in range(len(nums) - 1, -1, -1):
+        #     res[i] *= postfix
+        #     postfix *= nums[i]
+
+        # return res
+    
+    #----------------arrays of prefix and postfixes solution-------------
         # n = len(nums)
-        # answer = [1] * n
+        # prefix = [1] * n
+        # postfix = [1] * n
+        # res = [1] * n
 
-        # # Step 1: Left products
         # for i in range(1, n):
-        #     answer[i] = answer[i - 1] * nums[i - 1]
+        #     prefix[i] = prefix[i - 1] * nums[i - 1]
 
-        # # Step 2: Right products
-        # right_product = 1
-        # for i in range(n - 1, -1, -1):
-        #     answer[i] *= right_product
-        #     right_product *= nums[i]
+        # for i in range(n - 2, -1, -1):
+        #     postfix[i] = postfix[i + 1] * nums[i + 1]
 
-        # return answer
+        # for i in range(n):
+        #     res[i] = prefix[i] * postfix[i] 
+
+        # return res
 
 
-        res = [1] * len(nums)
 
-        prefix = 1
-        for i in range(len(nums)):
-            res[i] = prefix
-            prefix *= nums[i]
 
-        postfix = 1
-        for i in range(len(nums) - 1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
-
+    #-------------------brute force solution------------------------
+        n = len(nums)
+        res = [1] * n
+        for i in range(n):
+            prod = 1
+            for j in range(n):
+                if i != j:
+                    prod *= nums[j]
+            res[i] = prod
+        
         return res
+
 
 
 
