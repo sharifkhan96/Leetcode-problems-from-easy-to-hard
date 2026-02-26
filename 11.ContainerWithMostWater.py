a7ab6793c478cfd3a8678b87201956bbfd679567
Sheriff <78465304+sharifkhan96@users.noreply.github.com>
2026-02-26 23:27:07 +0000

    11-version2.0

--- 11.ContainerWithMostWater.py
+++ 11.ContainerWithMostWater.py
@@ -2,6 +2,7 @@
 # time and space complexity: O(n), and O(1)
 class Solution:
     def maxArea(self, height: list[int]) -> int:
+        # O(N)
         left = 0
         right = len(height) - 1
         max_area = 0
@@ -17,6 +18,17 @@ class Solution:
                 right -= 1
 
         return max_area
+    
+
+        # bruteforce solution: O(N^2) & O(1)
+        # max_area = 0
+        # for left in range(len(height)):
+        #     for right in range(left + 1, len(height)):
+        #         width = right - left
+        #         h = min(height[left], height[right])
+        #         max_area = max(max_area, width * h)
+
+        # return max_area
 
         
 
