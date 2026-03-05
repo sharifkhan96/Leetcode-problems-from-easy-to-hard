ef8c9a9ec3bf889e0632764535ac70f41bfcf9b3
Sheriff <78465304+sharifkhan96@users.noreply.github.com>
2026-03-05 23:59:41 +0000

    300

---
+++ 300.LongestIncreasingSubsequence.py
@@ -0,0 +1,25 @@
+
+class Solution:
+    def lengthOfLIS(self, nums: list[int]) -> int:
+        n = len(nums)
+        dp = [1] * n
+
+        for i in range(n):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+
+        return max(dp)
+
+
+# tiem and space complexities: O(m * n) and O(n) respectively
+
+def main():
+    nums = [10,9,2,5,3,7,101,18]
+
+    obj1 = Solution()
+    print(obj1.lengthOfLIS(nums))
+
+
+if __name__ == "__main__":
+    main()
