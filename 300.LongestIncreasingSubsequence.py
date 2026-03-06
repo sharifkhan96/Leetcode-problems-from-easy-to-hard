dfa56e746464252e1d814a68a853fa6686b9fc87
Sheriff <78465304+sharifkhan96@users.noreply.github.com>
2026-03-06 00:26:51 +0000

    300-VERSION2.0

--- 300.LongestIncreasingSubsequence.py
+++ 300.LongestIncreasingSubsequence.py
@@ -1,18 +1,29 @@
 
 class Solution:
     def lengthOfLIS(self, nums: list[int]) -> int:
+        # n = len(nums)
+        # dp = [1] * n
+
+        # for i in range(n):
+        #     for j in range(i):
+        #         if nums[j] < nums[i]:
+        #             dp[i] = max(dp[i], dp[j] + 1)
+
+        # return max(dp)
+
+
         n = len(nums)
-        dp = [1] * n
+        LIS = [1] * n
 
-        for i in range(n):
-            for j in range(i):
-                if nums[j] < nums[i]:
-                    dp[i] = max(dp[i], dp[j] + 1)
+        for i in range(n - 1, -1, -1):
+            for j in range(i + 1, n):
+                if nums[i] < nums[j]:
+                    LIS[i] = max(LIS[i], LIS[j] + 1)
 
-        return max(dp)
+        return max(LIS)
 
 
-# tiem and space complexities: O(m * n) and O(n) respectively
+# tiem and space complexities: O(n^2) & O(n)
 
 def main():
     nums = [10,9,2,5,3,7,101,18]
