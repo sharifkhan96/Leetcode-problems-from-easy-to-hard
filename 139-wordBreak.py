48a0fb3adf3f1c469759fa78a01895a2359705c6
Sheriff <78465304+sharifkhan96@users.noreply.github.com>
2026-03-11 15:13:38 +0000

    139-version2.0

--- 139-wordBreak.py
+++ 139-wordBreak.py
@@ -1,18 +1,33 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: list[str]) -> bool:
-        word_set = set(wordDict)  # O(1) lookups
-        n = len(s)
-        dp = [False] * (n + 1)
-        dp[0] = True  # base case: empty string
+        # classic forward dp
+        # word_set = set(wordDict)  # O(1) lookups
+        # n = len(s)
+        # dp = [False] * (n + 1)
+        # dp[0] = True  # base case: empty string
 
-        for i in range(1, n + 1):
-            for j in range(i):
-                if dp[j] and s[j:i] in word_set:
-                    dp[i] = True
-                    break  # no need to check further splits
+        # for i in range(1, n + 1):
+        #     for j in range(i):
+        #         if dp[j] and s[j:i] in word_set:
+        #             dp[i] = True
+        #             break  # no need to check further splits
 
-        return dp[n]
+        # return dp[n]
 
+        # classic backward dp
+        dp = [False] * (len(s) + 1)
+        dp[len(s)] = True
+
+        for i in range(len(s) - 1, -1, -1):
+            for w in wordDict:
+                if (i + len(w)) <= len(s) and s[i : i + len(w)] == w:
+                    dp[i] = dp[i + len(w)]
+                if dp[i]:
+                    break
+        return dp[0]
+
+# time complexity: O(n * m * k): n for outer loop, m for inner loop, k for slicing/comparison
+# space complexity: O(n) for dp list
 
 # Main function
 def main():
