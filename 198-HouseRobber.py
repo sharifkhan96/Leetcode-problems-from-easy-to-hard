d2097d224fb32a02aafa147afbba5c5eb9e13556
Sheriff <78465304+sharifkhan96@users.noreply.github.com>
2026-03-14 23:29:48 +0000

    198-version2.0

--- 198-HouseRobber.py
+++ 198-HouseRobber.py
@@ -1,20 +1,51 @@
 class Solution:
+    
+    # used for recursion+memoization solution
+    def __init__(self):
+        self.memo = {}
+
+
+    # recursion + memoization
     def rob(self, nums: list[int]) -> int:
-        n = len(nums)
+        self.memo = {}
 
-        if n == 0:
+        return self.robFrom(0, nums)
+    
+    def robFrom(self, index, nums):
+        # base case
+        if index >= len(nums):
             return 0
-        if n == 1:
-            return nums[0]
         
-        dp = [0] * n
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
+        # return cached values
+        if index in self.memo:
+            return self.memo[index]
+        
+        # actual recursion
+        ans = max(
+                    self.robFrom(index + 1, nums), self.robFrom(index + 2, nums) + nums[index]
+                  
+                  )
+        # cache for future use
+        self.memo[index] = ans
+        return ans
+        
+    
+    # def rob(self, nums: list[int]) -> int:
+    #     n = len(nums)
 
-        for i in range(2, n):
-            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
+    #     if n == 0:
+    #         return 0
+    #     if n == 1:
+    #         return nums[0]
+        
+    #     dp = [0] * n
+    #     dp[0] = nums[0]
+    #     dp[1] = max(nums[0], nums[1])
 
-        return dp[n-1]
+    #     for i in range(2, n):
+    #         dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
+
+    #     return dp[n-1]
 
         
 #time comnp:O(n)
@@ -25,11 +56,11 @@ space comp of o(1)
         prev1, prev2 = 0, 0
 
         for num in nums:
-            temp = max(prev1, num + prev2)
-            prev2 = prev1
-            prev1 = temp
+            temp = max(prev1 + num, prev2)
+            prev1 = prev2
+            prev2 = temp
         
-        return prev1
+        return prev2
 '''
 
 def main():
