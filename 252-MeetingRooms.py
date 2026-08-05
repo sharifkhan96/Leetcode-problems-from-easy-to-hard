3992827766b29c10ea05f6b48e1087d4c7d18446
sha <78465304+sharifkhan96@users.noreply.github.com>
2026-08-05 04:32:56 +0100

    252- version2.0

--- 252-MeetingRooms.py
+++ 252-MeetingRooms.py
@@ -1,21 +1,35 @@
 class Solution:
     def canAttendMeetings(self, intervals: list[list[int]]) -> bool:
-        def overlap(interval1: list[int], interval2: list[int]) -> bool:
-            return (interval1[0] >= interval2[0] and interval1[0] < interval2[1]
-                or interval2[0] >= interval1[0] and interval2[0] < interval1[1])
+        # algo
+        # sort the intervals
+        intervals.sort(key=lambda x: x[0])
+        
+        # travers =
+        for i in range(len(intervals)-1):
+            # if end of ith index value is less than or eq to # # start of ith+1 index value: no overlap, cont till # end: true
 
-        for i in range(len(intervals)):
-            for j in range(i + 1, len(intervals)):
-                if overlap(intervals[i], intervals[j]):
-                    return False
+            if intervals[i][1] <= intervals[i+1][0]:
+                continue
+            else:
+                return False
+
+        # otherwise overlap, return false
         return True
 
+        # def overlap(interval1: List[int], interval2: List[int]) -> bool:
+        #     return (interval1[0] >= interval2[0] and interval1[0] < interval2[1]
+        #         or interval2[0] >= interval1[0] and interval2[0] < interval1[1])
+
+        # for i in range(len(intervals)):
+        #     for j in range(i + 1, len(intervals)):
+        #         if overlap(intervals[i], intervals[j]):
+        #             return False
+        # return True
 
 
 
-
-# Time complexity: O(n^2)
-# Space Complexity: O(1)
+# Time complexity: O(n log n)
+# Space Complexity: O(n log n)
 
 def main():
     intervals = [[0,30],[5,10],[15,20]]
