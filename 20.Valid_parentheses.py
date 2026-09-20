1c406b6a9559220d331c07923661e63a72e88766
sha <78465304+sharifkhan96@users.noreply.github.com>
2026-09-20 01:40:56 +0100

    V2.0

--- 20.Valid_parentheses.py
+++ 20.Valid_parentheses.py
@@ -1,33 +1,50 @@
+import re
 class Solution:
 
     def isValid(self, s: str) -> bool:
-        
-        stack1 = []
-        hashmap1 = {')': '(', ']': '[', '}': '{'}
-        
-        for element in s:
-            if stack1 and (element in hashmap1 and stack1[-1] == hashmap1[element]):
+
+        '''
+        stack86 = []
+
+        hashmap86 = {")":"(", "}":"{","]":"["}
+
+        for char in s:
+            if char in hashmap86:
+                if stack86 and stack86[-1] == hashmap86[char]:
+                    stack86.pop()
+                else:
+                    return False
+            else:
+                stack86.append(char)
+        return not stack86
+        '''
+
+        '''
+        for char in s:
+            if char in hashmap1.values():
+                stack1.append(char)
+            elif char in bracket_map:
+                if not stack1 or stack[-1] != hashmap1[char]:
+                    return False
                 stack1.pop()
             else:
-                stack1.append(element)
+                continue
 
-        return not stack1
-
-        # for char in s:
-        #     if char in hashmap1.values():
-        #         stack1.append(char)
-        #     elif char in bracket_map:
-        #         if not stack1 != hashmap1[char]:
-        #             return False
-        #         stack1.pop()
-        #     else:
-        #         continue
-
-        # return len(stack1) == 0
-
+        return len(stack1) == 0
+        '''
 
 # time: o(n)
 # space: o(n)
+
+        
+        p = re.compile("\[\]|\(\)|\{\}")
+        changed = 1
+        while (s.__sizeof__() and changed):
+            s, changed = p.subn("", s)
+            
+        if len(s) == 0 : return True
+        return False
+# o(n^2)
 
 #note: we CAN solve it via a second approach, jsut by tweaking the logic .. 
 
